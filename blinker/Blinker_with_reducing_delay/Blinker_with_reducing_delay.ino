367acfbcf4ac1ad56562a59df50371a203016c78
Krishna Noutiyal <krishnanoutiyal@gmail.com>
2025-02-08 15:11:06 +0530

    Initial Commit

---
+++ blinker/Blinker_with_reducing_delay/Blinker_with_reducing_delay.ino
@@ -0,0 +1,51 @@
+
+
+int DELAY = 500; // Initail delay
+int RESET = DELAY; // Variable to reset to default delay
+int DECREMENT = 500; // value to be decresed each time
+
+
+int n = 0;
+
+int RED = 10; // PINOUT for RED LED
+int YELLOW = 9; // PINOUT for YELLOW LED
+int GREEN = 6; // PINOUT for GREEN LED
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(RED,OUTPUT);
+  pinMode(YELLOW,OUTPUT);
+  pinMode(GREEN,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  if (n >=DECREMENT*0.1){
+    DELAY = RESET;
+    n = 0;
+  }
+  else if (DELAY <=100 ){
+    DELAY = 100;
+    n++;
+  }
+  else if (DELAY <= DECREMENT){
+    DELAY = DECREMENT;
+    DECREMENT = DECREMENT*0.1;
+  }
+  digitalWrite(RED,1);
+  delay(DELAY);
+
+  digitalWrite(YELLOW,1);
+  digitalWrite(RED,0);
+  delay(DELAY);
+
+  digitalWrite(YELLOW,0);
+  digitalWrite(GREEN,1);
+  delay(DELAY);
+  digitalWrite(GREEN,0);
+
+  DELAY -= DECREMENT;
+
+
+}
---
+++ blinker/Blinker_with_reducing_delay/README.md.txt
@@ -0,0 +1,11 @@
+# Blinker With Reducing Delay
+---
+
+This Arduino code is for 3 LEDs that light up one by one and the delay between the TURN OFF and TURN ON signal, decreases each time. You can set the DELAY and amount of DECREMENT in the code. exponential decrease in DELAY problem has been taken care off and the code resets to the default DELAY specified by the user that the start of the code.
+
+---
+# How the looks in real life ?
+---
+Play the below video to see how the blinker work in real life.
+
+[../../assets/blinker01.mp4]
